78e236fdd24ee3cb1d67bd3aac9086bcdae6c4c3
gabriel <gabrielsouzat2005@outlook.com>
2024-06-03 08:23:53 -0300

    Add project files.

---
+++ ReactApp1.Server/Data/Context.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+
+namespace ReactApp1.Server.Data
+{
+    public class Context : DbContext 
+    {
+        public Context() : base("name=Context")
+        {
+            Database.SetInitializer<Context>(null);
+        }
+
+        public static Context Create()
+        {
+            return new Context();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add<Context>(new Context());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //DbSet
+    }
+}
